e3187228ade5f8b303e53a1776a298341502d400
Mark Boddington <mark.boddington@nginx.com>
2021-02-15 14:53:05 +0000

    loop, retry license and registrations

--- terraform/azure/controller_custom_data.sh
+++ terraform/azure/controller_custom_data.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+function put_license() {
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Putting Controller License"
+  curl -kvv -X PUT -f -b /var/run/cloud-init/cookie.jar \
+    -H 'Content-type: application/json' \
+    -d '{
+      "metadata": {
+        "name": "license"
+      }, "desiredState": {
+        "content": "${controller_token}"
+      }}' \
+    https://${hostname}.${domain}/api/v1/platform/license > /var/run/cloud-init/ctrl-license.log
+    return $?
+}
+
 function license_controller() {
   
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S ==================== LICENSE ============================"
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Controller Login"
   curl -kvv -X POST -f -c /var/run/cloud-init/cookie.jar \
     -H 'Content-type: application/json' \
     -d '{ 
@@ -13,28 +32,64 @@ function license_controller() {
     https://${hostname}.${domain}/api/v1/platform/login
 
   grep session /var/run/cloud-init/cookie.jar >/dev//null 2>&1 || return 1
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Controller Login SUCCESS"
 
-  curl -kvv -X PUT -f -b /var/run/cloud-init/cookie.jar \
-    -H 'Content-type: application/json' \
-    -d '{
-      "metadata": {
-        "name": "license"
-      }, "desiredState": {
-        "content": "${controller_token}"
-      }}' \
-    https://${hostname}.${domain}/api/v1/platform/license
-
-    return $?
+  put_license
+  result=1
+  completed=0
+  attempts=0
+  while [ $completed -eq 0 ] 
+  do
+    date +"%Y-%m-%d %H:%M:%S ========================================================="
+    date +"%Y-%m-%d %H:%M:%S Controller License status GET"
+    curl -kvv -f -b /var/run/cloud-init/cookie.jar https://${hostname}.${domain}/api/v1/platform/license > /var/run/cloud-init/ctrl-license.log
+    grep '"isError":true' /var/run/cloud-init/ctrl-license.log >/dev/null
+    if [ $? -eq 0 ]
+    then
+      date +"%Y-%m-%d %H:%M:%S ========================================================="
+      date +"%Y-%m-%d %H:%M:%S Controller License status ERROR (Putting new license)"
+      put_license
+      sleep 5
+    fi
+    grep '"isConfigured":true' /var/run/cloud-init/ctrl-license.log >/dev/null
+    if [ $? -eq 0 ]
+    then
+      date +"%Y-%m-%d %H:%M:%S ========================================================="
+      date +"%Y-%m-%d %H:%M:%S Controller License status SUCCESS"
+      completed=1 
+      result=0
+    elif [ $attempts -gt 3 ]
+    then
+      date +"%Y-%m-%d %H:%M:%S ========================================================="
+      date +"%Y-%m-%d %H:%M:%S Controller License status FAILED"
+      completed=1 
+      result=1
+    else
+      attempts=$(( $attempts + 1 ))
+      sleep 10
+    fi
+  done
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Controller License Result: $result"
+  return $result
 }
 
+date +"%Y-%m-%d %H:%M:%S ========================================================="
+date +"%Y-%m-%d %H:%M:%S Setting Hostname"
 echo ${hostname} > /etc/hostname
 echo "${ipaddr}  ${hostname} ${hostname}.${domain}" >> /etc/hosts
 hostname ${hostname}
 
 if [ "${install_needed}" == "true" ]
 then
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Install Needed - Installing Controller"
   su - nginx -c '/opt/nginx-install/controller_install.sh "${install_needed}" "${hostname}.${domain}" "${controller_admin_user}" "${controller_admin_pass}"'
 else
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Controller Installed by Packer"
+  date +"%Y-%m-%d %H:%M:%S Starting Kubernetes"
   mkdir /home/${username}/.kube
   cp /etc/kubernetes/admin.conf /home/${username}/.kube/config
   chown -R ${username} /home/${username}/.kube
@@ -43,14 +98,25 @@ else
   systemctl enable kubelet.service
 
   # Give controller a couple of minutes to startup
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Sleeping for 2 minutes"
   sleep 120
 fi
 
 if [ "${controller_token}" != "" ]
 then
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Starting Licensing Loop...."
   until license_controller
-  do 
+  do
+    date +"%Y-%m-%d %H:%M:%S ========================================================="
+    date +"%Y-%m-%d %H:%M:%S ==================== SLEEPING ==========================="
+    date +"%Y-%m-%d %H:%M:%S ========================================================="
     sleep 10
   done
   rm /var/run/cloud-init/cookie.jar
 fi
+
+date +"%Y-%m-%d %H:%M:%S ========================================================="
+date +"%Y-%m-%d %H:%M:%S ==================== COMPLETE ==========================="
+date +"%Y-%m-%d %H:%M:%S ========================================================="
--- terraform/azure/nginx_custom_data.sh
+++ terraform/azure/nginx_custom_data.sh
@@ -2,7 +2,10 @@
 
 function register_with_controller() {
 
-  curl -k -sS -L https://${controller_name}.${domain}/install/controller-agent > /var/run/cloud-init/install.sh
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S ==================== REGISTER ==========================="
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Controller Login"
   curl -kvv -X POST -f -c /var/run/cloud-init/cookie.jar \
     -H 'Content-type: application/json' \
     -d '{ 
@@ -14,30 +17,93 @@ function register_with_controller() {
     https://${controller_name}.${domain}/api/v1/platform/login
 
   grep session /var/run/cloud-init/cookie.jar >/dev//null 2>&1 || return 1
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Controller Login SUCCESS"
 
-  API_KEY=$(curl -kvv -f -b /var/run/cloud-init/cookie.jar \
-    https://${controller_name}.${domain}/api/v1/platform/global | jq .desiredState.agentSettings.apiKey)
-  export API_KEY
+  result=1
+  completed=0
+  attempts=0
+  while [ $completed -eq 0 ]
+  do
+    date +"%Y-%m-%d %H:%M:%S ========================================================="
+    date +"%Y-%m-%d %H:%M:%S Controller License status GET"
+    curl -kvv -f -b /var/run/cloud-init/cookie.jar https://${controller_name}.${domain}/api/v1/platform/license > /var/run/cloud-init/ctrl-license.log
+    grep '"isConfigured":true' /var/run/cloud-init/ctrl-license.log >/dev/null
+    if [ $? -eq 0 ]
+    then
+      date +"%Y-%m-%d %H:%M:%S ========================================================="
+      date +"%Y-%m-%d %H:%M:%S Controller License status SUCCESS"
+      completed=1
+      result=0
+    elif [ $attempts -gt 3 ]
+    then
+      date +"%Y-%m-%d %H:%M:%S ========================================================="
+      date +"%Y-%m-%d %H:%M:%S Controller License status FAILED"
+      completed=1
+      result=1
+    else
+      attempts=$(( $attempts + 1 ))
+      sleep 10
+    fi
+  done
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Controller License Result: $result"
 
-  if [ "${nginx_location}" == "" ]
+  if [ $result -eq 0 ]
   then
-    sh /var/run/cloud-init/install.sh -i ${hostname} -l unspecified
-    return $?
-  else
-    curl -kvv -X PUT -f -b /var/run/cloud-init/cookie.jar \
-      -H 'Content-type: application/json' \
-      -d '{
-        "metadata": {
-          "name": "${nginx_location}"
-        }, "desiredState": {
-          "type": "OTHER_LOCATION"
-        }}' \
-      https://${controller_name}.${domain}/api/v1/infrastructure/locations/${nginx_location}
+
+    date +"%Y-%m-%d %H:%M:%S ========================================================="
+    date +"%Y-%m-%d %H:%M:%S Controller Download Installer Script"
+    curl -f -k -sS -L https://${controller_name}.${domain}/install/controller-agent > /var/run/cloud-init/install.sh
+    result=$?
+
+    if [ $result -eq 0 ]
+    then
+
+      date +"%Y-%m-%d %H:%M:%S ========================================================="
+      date +"%Y-%m-%d %H:%M:%S Controller Download Installer Script - SUCCESS"
+      date +"%Y-%m-%d %H:%M:%S Controller Getting API Key"
+
+      API_KEY=$(curl -kvv -f -b /var/run/cloud-init/cookie.jar \
+        https://${controller_name}.${domain}/api/v1/platform/global | jq .desiredState.agentSettings.apiKey)
+      export API_KEY
+
+      if [ "${nginx_location}" == "" ]
+      then
+        date +"%Y-%m-%d %H:%M:%S ========================================================="
+        date +"%Y-%m-%d %H:%M:%S NGINX Register. Location: unspecified"
+        sh /var/run/cloud-init/install.sh -i ${hostname} -l unspecified
+        result=$?
+      else
+        date +"%Y-%m-%d %H:%M:%S ========================================================="
+        date +"%Y-%m-%d %H:%M:%S NGINX Location: ${nginx_location} (creating)"
+        curl -kvv -X PUT -f -b /var/run/cloud-init/cookie.jar \
+          -H 'Content-type: application/json' \
+          -d '{
+            "metadata": {
+              "name": "${nginx_location}"
+            }, "desiredState": {
+              "type": "OTHER_LOCATION"
+            }}' \
+          https://${controller_name}.${domain}/api/v1/infrastructure/locations/${nginx_location}
     
-    [ $? -ne 0 ] && return 1
-    sh /var/run/cloud-init/install.sh -i ${hostname} -l ${nginx_location}
-    return $?
+        result=$?
+        if [ $result -eq 0 ] 
+        then
+          date +"%Y-%m-%d %H:%M:%S ========================================================="
+          date +"%Y-%m-%d %H:%M:%S NGINX Register. Location: ${nginx_location}"
+          sh /var/run/cloud-init/install.sh -i ${hostname} -l ${nginx_location}
+          result=$?
+        fi
+      fi
+    else
+      date +"%Y-%m-%d %H:%M:%S ========================================================="
+      date +"%Y-%m-%d %H:%M:%S Controller Download Installer Script - FAILED"
+    fi
   fi
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S NGINX Registration Result: $result"
+  return $result
   
 }
 
@@ -47,9 +113,18 @@ hostname ${hostname}
 
 if [ "${controller_token}" != "" ]
 then
+  date +"%Y-%m-%d %H:%M:%S ========================================================="
+  date +"%Y-%m-%d %H:%M:%S Starting Registration Loop...."
   until register_with_controller
   do 
+    date +"%Y-%m-%d %H:%M:%S ========================================================="
+    date +"%Y-%m-%d %H:%M:%S ==================== SLEEPING ==========================="
+    date +"%Y-%m-%d %H:%M:%S ========================================================="
     sleep 10
   done
   rm /var/run/cloud-init/cookie.jar
 fi
+
+date +"%Y-%m-%d %H:%M:%S ========================================================="
+date +"%Y-%m-%d %H:%M:%S ==================== COMPLETE ==========================="
+date +"%Y-%m-%d %H:%M:%S ========================================================="
