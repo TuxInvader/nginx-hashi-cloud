2f54ba26df5a88582e440ac6cb4444ba731d022f
Mark Boddington <mark.boddington@nginx.com>
2021-02-17 14:47:16 +0000

    Wait for k8s to quiet down after install

--- packer/azure/controller_install.sh
+++ packer/azure/controller_install.sh
@@ -14,8 +14,22 @@ tar zxvpf controller-installer.tar.gz
 until controller-installer/install.sh -n -m 127.0.0.1 -x 12321 -b false -g false -j $CONTROLLER_USERNAME -e $CONTROLLER_USERNAME -p $CONTROLLER_PASSWORD -f $CONTROLLER_HOSTNAME -t $CONTROLLER_FIRSTNAME -u $CONTROLLER_SURNAME -c -w -y --configdb-volume-type local --tsdb-volume-type local -a NGINX ; do sleep 10 ; done
 
 # Sleep to ensure all the pods have settled, before shutting down.
+# Exit after 20 mins or when the load drops below 2.0 for 2 consecutive minutes
 echo "Sleeping while K8s settles"
-for sleeps in {01..10}; do date "+ %H:%M:%S Sleep $sleeps" ; uptime ; sleep 60 ; done
+for sleeps in {01..20}
+do 
+  load=$(uptime | sed -re 's/.*average: ([^,]+).*/\1/')
+  date "+ %H:%M:%S Sleep $sleeps - Load $load"
+  if [ $sleeps -gt 3 ]
+  then
+    if [[ "$load" =~ [01]. ]] && [[ "$lastload" =~ [01.] ]]
+    then
+      break
+    fi
+  fi
+  lastload=$load
+  sleep 60
+done
 
 # Disable kubernetes at startup. We'll re-enable with cloud-init
 sudo systemctl disable kubelet.service
